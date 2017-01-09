ced5adbbcb337e4cf8f863050ba9f68985dd61db
SugendhKGanga <SugendhKGanga@users.noreply.github.com>
2017-01-09 18:52:18 +0530

    Update initenv.sh

--- initenv.sh
+++ initenv.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-
+n=1
 for (( c=1; c<=scaled_to; c++ ))
 do
-    n=1
-    n=$(( n+1 ))
-
     docker-cloud exec trac-app-$n trac-admin /opt/test initenv test mysql://root:mysql@mysql-for-trac-$n/trac
+    n=$(( n+1 ))
 done
